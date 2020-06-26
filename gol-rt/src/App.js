ef1c45605955c2de3e546d3a59bb067d97b05e7d
raj u <r@Rs-MacBook-Air.local>
2020-06-26 01:44:18 -0700

    It works

---
+++ gol-rt/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import produce from 'inner';
+
+
+const numRows = 50;
+const numCols = 50
+;
+function App() {
+  
+
+  return <div>hi</div>
+}
+
+export default App;
